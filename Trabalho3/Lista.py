aa7330087427d533db96ad275a68e991b195aeb2
brunomesss <89047858+brunomesss@users.noreply.github.com>
2021-12-05 16:15:47 -0300

    trabalho4

--- Trabalho3/Lista.py
+++ Trabalho3/Lista.py
@@ -1,75 +1,74 @@
-de  Sem  importação  Não
+from No import No
 
-classe  ListaDuplaEncadeada :
+class ListaDuplaEncadeada:
 
-    def  __init__ ( self ):
-        eu . inicio  =  Nenhum
-        eu . fim  =  Nenhum
-        eu . tamanho  =  0
+    def __init__(self):
+        self.inicio = None
+        self.fim = None
+        self.tamanho = 0
 
     
-    def  adicionar ( próprio , dado ):
-
-        # Cria um novo no local para Nenhum (anterior e proximo)
-        não  =  Não ( dado , nenhum , nenhum )
-
-        # Se o inicio para None a lista esta vazia
-        # O inicio e o fim obtem o novo não
-        se  eu . inicio  é  nenhum :
-            eu . inicio  =  não
-            eu . fim  =  não
-
-        # Caso ja exista algum valor na lista
-        mais :
-            # O anterior aponta para o fim
-            não . anterior  =  self . fim
-            # O proximo aponta para Nenhum
-            não . proximo  =  Nenhum
-            # O proximo do fim sempre aponta para o novo no
-            eu . fim . proximo  =  não
-            # O fim passa a ser o novo não
-            eu . fim  =  não
-
-
-    def  imprimir ( próprio ):
-        
-        print ( "Lista duplamente encadeada na ordem dos elementos adequados:" )
-
-        # O não atual e o primeiro no lista
-        no_atual  =  self . inicio
-
-        não  =  ""
-        # Para cada no valido da lista
-        enquanto  no_atual  não é  nenhum : 
-            se  não_atual . anterior  é  nenhum :
-                não  + =  "Nenhum"
-            não  + =  "<---> |"  +  str ( no_atual . dado ) +  "|"
-            se  não_atual . proximo  é  nenhum :
-                não  + =  "<---> Nenhum"
-
-            no_atual  =  no_atual . proximo
-        imprimir ( não )
-        imprimir ( "="  *  110 )
-
-
-
-    def  imprimir_invertido ( self ):
+    def adicionar(self, dado):
 
         
-        print ( "Lista duplamente encadeada em ordem invertida:" )
+        no = No(dado, None, None)
 
-        # O no atual e o ultimo no da lista
-        no_atual  =  self . fim
+        
+        if self.inicio is None:
+            self.inicio = no
+            self.fim = no
 
-        não  =  ""
-        # Para cada no valido da lista
-        enquanto  no_atual  não é  nenhum : 
-            se  não_atual . proximo  é  nenhum :
-                não  + =  "Nenhum"
-            não  + =  "<---> |"  +  str ( no_atual . dado ) +  "|"
-            se  não_atual . anterior  é  nenhum :
-                não  + =  "<---> Nenhum"
+        
+        else:
+            
+            no.anterior = self.fim
+            
+            no.proximo = None
+            
+            self.fim.proximo = no
+           
+            self.fim = no
 
-            no_atual  =  no_atual . anterior
-        imprimir ( não )
-        imprimir ( "="  *  110 )
+
+    def imprimir(self):
+        
+        print("Lista duplamente encadeada na ordem dos elementos adicionados: ")
+
+        
+        no_atual = self.inicio
+
+        no = ""
+        
+        while no_atual is not None:
+            if no_atual.anterior is None:
+                no += "None "
+            no += " | " + str(no_atual.dado) + " | "
+            if no_atual.proximo is None:
+                no += "None"
+
+            no_atual = no_atual.proximo
+        print(no)
+        print()
+
+
+
+    def imprimir_invertido(self): 
+
+        
+        print("Lista duplamente encadeada em ordem invertida: ")
+
+        
+        no_atual = self.fim
+
+        no = ""
+        
+        while no_atual is not None:
+            if no_atual.proximo is None:
+                no += "None "
+            no += " | " + str(no_atual.dado) + " | "
+            if no_atual.anterior is None:
+                no += "None"
+
+            no_atual = no_atual.anterior
+        print(no)
+        print()
--- Trabalho3/Main.py
+++ Trabalho3/Main.py
@@ -4,11 +4,11 @@ from Lista import ListaDuplaEncadeada
 l1 = ListaDuplaEncadeada()
 
 
-l1.adicionar( "Chocolate" )
-l1.adicionar( "Sorvete" )
-l1.adicionar( "Cupcake" )
-l1.adicionar( "Brigadeiro" )
-l1.adicionar( "Brownie" )
+l1.adicionar( "Machado" )
+l1.adicionar( "Serrote" )
+l1.adicionar( "Toneira" )
+l1.adicionar( "Martelo" )
+l1.adicionar( "Colher de Pedreiro" )
 
 l1.imprimir()
 l1.imprimir_invertido()
